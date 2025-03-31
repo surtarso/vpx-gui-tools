10c256386555f29ad356cab8623a98501a8cac4a
surtarso <tarso.galvao@gmail.com>
2025-03-31 07:57:59 -0300

    updated version call

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc == 2 && std::string(argv[1]) == "--version") {
-        std::cout << "ASAPCabinetFE version " << PROJECT_VERSION << std::endl;
+        std::cout << "VPXGUITools version " << PROJECT_VERSION << std::endl;
         return 0;
     }
 
