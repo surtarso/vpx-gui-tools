1009047048441f451c036f13a29eb3fc2d8f985b
surtarso <tarso.galvao@gmail.com>
2025-03-31 06:22:30 -0300

    added --version and cicd actions

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include "core/application.h"
 #include "utils/logging.h"
+#include "version.h"
 #include <SDL.h>
 #include <filesystem>
 #include <iostream>
+#include <string>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc == 2 && std::string(argv[1]) == "--version") {
+        std::cout << "ASAPCabinetFE version " << PROJECT_VERSION << std::endl;
+        return 0;
+    }
+
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         LOG_DEBUG("SDL_Init Error: " << SDL_GetError());
         return 1;
